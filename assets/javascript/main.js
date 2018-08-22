da3cbac5a757bc1747180911ec641366a9026ed2
rmaharja@gmail.com <rmaharja@gmail.com>
2018-08-21 23:52:05 -0400

    finished basic html css

---
+++ assets/javascript/main.js
@@ -0,0 +1,18 @@
+//Grab the references
+
+//Variables:
+//wordBank, wins, losses, picked word, guesses left,, game running, picked word placebolder, guessed letter bank, incorrect letter bank...
+
+//newGame function to reset all stats, pick new word and creat placeholders
+
+//letterGuess function, takes in the letter you pressed and sees if it's in the selected word
+
+//checkIncorrect letter
+
+//check if losing
+
+//check if we win
+
+//Add even listener for new game button
+
+//Add onkeyup even to trigger letterguess
